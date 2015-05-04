0ad5a98aed43095e59636c37d9562b563ba3a71d
Daniel Vogel <dvogel@cs.stanford.edu>
2015-05-03 23:12:58 -0400

    remove long if

--- dl.rb
+++ dl.rb
@@ -28,7 +28,11 @@ def sanitize_filename(filename)
   if !filename.nil?
     return filename.to_s.gsub(/[\r\n]/,'').gsub(/[^0-9A-Za-z%.\,'\-_ \(\)\[\]\&\+]/, '_').gsub(/_+/,'_').gsub(/_$/,'')
   end
+end
 
+def log_die(msg)
+  puts msg
+  Process.exit 1
 end
 
 errors = ""
@@ -47,52 +51,51 @@ p_success = form.submit
 
 html_doc = Nokogiri::HTML(p_success.body)
 
-if html_doc.css("h2").text == 'You Are Logged In'
-  puts "Logged in"
+log_die("Unable to log in") if html_doc.css("h2").text != 'You Are Logged In'
 
-  # get list of courses
-  hash = Hash.new
-  a.get('https://lh.hbs.edu').body.scan(/<a href="\/d2l\/lp\/ouHome\/home.d2l\?ou=(\d+)" title="Enter ([^"]+)">/) {|x,y| hash[x] = y }
-  puts "You are enrolled in the following courses"
-  hash.keys.each do |x|
-    puts "#{x} #{hash[x]}"
-  end
+puts "Logged in"
 
-  p_toc = a.get(url).body
-  toc_doc = Nokogiri::HTML(p_toc)
-  cnt = 1
-  toc_doc.css('tr.d_ggl1').each do |itm|
-    i_title = itm.css('td.d_gn').text
-    puts sanitize_filename i_title
-    i_lastchild = itm.parent.last_element_child
-    i_child = itm.next_element
-    has_validchild=false
-    while !i_child.nil? && i_child['class'] != 'd_ggl1' && i_child != i_lastchild
-      has_validchild=true
-      out_curfolder = out_folder+('%03d ' % cnt.to_s)+sanitize_filename(i_title)+'/'
-      FileUtils.mkpath(out_curfolder) unless Dir.exists?(out_curfolder)
-      dl_link = i_child.css('a.D2LLink')
-      puts " > " + dl_link.text
-      dl_file = preview_url + dl_link.attr('onclick').text.match(/PreviewTopic\((\d+)\,/)[1]
-      puts " >> " + dl_file
-      content_url = Nokogiri::HTML(a.get(dl_file).body).css('#frContentFile').attr('src').text
-      puts " >+ " + content_url
-      begin
-        file_content = a.get(content_url)
-        file_type = fm.buffer(file_content.body)
-        file_ext = MIME::Types[file_type].first.extensions.first 
-        puts " ># " + file_type+ ' --> ' + file_ext
-        file_content.save(out_curfolder+sanitize_filename(dl_link.text)+'.'+file_ext)
-      rescue
-        errors << content_url + "\n"
-      end
+# get list of courses
+hash = Hash.new
+a.get('https://lh.hbs.edu').body.scan(/<a href="\/d2l\/lp\/ouHome\/home.d2l\?ou=(\d+)" title="Enter ([^"]+)">/) {|x,y| hash[x] = y }
+log_die("Unable to find courses") if hash.length == 0
+puts "You are enrolled in the following courses"
+hash.keys.each do |x|
+  puts "#{x} #{hash[x]}"
+end
 
-      i_child = i_child.next_element
+p_toc = a.get(url).body
+toc_doc = Nokogiri::HTML(p_toc)
+cnt = 1
+toc_doc.css('tr.d_ggl1').each do |itm|
+  i_title = itm.css('td.d_gn').text
+  puts sanitize_filename i_title
+  i_lastchild = itm.parent.last_element_child
+  i_child = itm.next_element
+  has_validchild=false
+  while !i_child.nil? && i_child['class'] != 'd_ggl1' && i_child != i_lastchild
+    has_validchild=true
+    out_curfolder = out_folder+('%03d ' % cnt.to_s)+sanitize_filename(i_title)+'/'
+    FileUtils.mkpath(out_curfolder) unless Dir.exists?(out_curfolder)
+    dl_link = i_child.css('a.D2LLink')
+    puts " > " + dl_link.text
+    dl_file = preview_url + dl_link.attr('onclick').text.match(/PreviewTopic\((\d+)\,/)[1]
+    puts " >> " + dl_file
+    content_url = Nokogiri::HTML(a.get(dl_file).body).css('#frContentFile').attr('src').text
+    puts " >+ " + content_url
+    begin
+      file_content = a.get(content_url)
+      file_type = fm.buffer(file_content.body)
+      file_ext = MIME::Types[file_type].first.extensions.first 
+      puts " ># " + file_type+ ' --> ' + file_ext
+      file_content.save(out_curfolder+sanitize_filename(dl_link.text)+'.'+file_ext)
+    rescue
+      errors << content_url + "\n"
     end
-    cnt = cnt+1 unless !has_validchild
+
+    i_child = i_child.next_element
   end
-else
-  puts "Login error!"
+  cnt = cnt+1 unless !has_validchild
 end
 
 if errors != "" 
